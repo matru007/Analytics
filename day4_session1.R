d0ca7d3a7351dc9eb04cca70c8775535fc2f85af
matru007 <matrupnanda@gmail.com>
2019-03-17 11:53:01 +0530

    r

--- day4_session1.R
+++ day4_session1.R
@@ -17,9 +17,41 @@ course
 #give the data frame as dataiitb
 #find properties of data like
 #first row or last row
+data2 = read.csv('https://raw.githubusercontent.com/dupadhyaya/hheanalytics/master/data/dataiitb.csv')
 #dimensions
 cm(passfail)
 table(passfail) # no of pass fails
+head(data2)
+library(dplyr)
+data2 %>% group_by(gender) %>% summarise(meanM1 = mean(marks1), meanM2 = mean(marks2))
+data2 %>% group_by(course) %>% summarise(meanM1 = mean(marks1), meanM2 = mean(marks2))
+
+data2 %>% group_by(gender,course) %>% summarise(meanM1 = mean(marks1),meanM2 = mean(marks2), minA = min(age), sdA = sd(age))%>%
+  arrange(meanM1)
+data2 %>% group_by(gender,course) %>% summarise(meanM1 = mean(marks1),meanM2 = mean(marks2), minA = min(age), sdA = sd(age), count =n())%>%arrange(gender, desc(sdA))
+
+data2 %>% group_by(gender) %>% arrange(gender,marks2) %>% top_n(2,marks2)
+barplot(height= c(10,20))
+table(data2$gender)
+barplot(height = table(data2$gender), col = 1:2)
+barplot(height = table(data2$course))
+fit1 = lm(marks2~ age + marks1 +course +gender, data = data2)
+summary(fit1)
+
+library(rpart.plot)
+fit1 =rpart(passfail ~ marks1 + marks2 +gender, data = data2,method = "class")
+
+rpart.plot(fit1)
+
+fit1 = kmeans(data2[,c('age','marks1','marks2')], centers = 4)
+fit1$centers
+
+
+# %>% piping operator
+
+
+
+
 
 dataiitb = data.frame(sname,gender,age,marks1,marks2,passfail)
 dataiitb
